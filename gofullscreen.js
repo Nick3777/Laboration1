24efa8c6f941a2d2375015a07fd2add6d3097d22
hannes3000 <moritz.hansen@mailbox.org>
2023-02-16 20:20:24 +0100

    Added svg at /home + minor stuff added fullscreen capabilities @ /hannes + cool gif :D done! 👌👌👌

--- gofullscreen.js
+++ gofullscreen.js
@@ -1,5 +1,6 @@
 function gofullscreen(pic){
     let plane = document.getElementById('fsplane');
     plane.style.backgroundImage = 'url(' + pic + ')';
+    plane.style.top = document.documentElement.scrollTop.toString() + 'px';
     plane.style.display = 'block';
 }
